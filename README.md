94067b7c288ee502fe47cf6416d8116e094a1898
Leonardo Ferreira <leoventura245@gmail.com>
2024-09-11 20:58:20 -0300

    api finalizada

--- README.md
+++ README.md
@@ -5,5 +5,8 @@ api que retorna a requisicao do total de vendas
 
 apis se conectam com o seu código por meio de JSON.
 
+lembrando que agora com a api pronta, é preciso criar um outro código que faz uma requisição pra esta api, com isso o usuario pode utilizar de maneira útil a informação.
+    - utilizar a lib requests do py para poder fazer a requisição
+
 importar flask
 importar pandas
--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import pandas as pd
+from flask import Flask, jsonify
 
-tabela = pd.read_csv('dados.csv')
-total_vendas = tabela['Vendas'].sum()
-print(total_vendas)
+app = Flask(__name__) # inicializando o aplicativo, padrao flask
+
+# construindo as funcionalidades
+@app.route('/') # decorator do link, ex: /paginainicial. so a barra ja seria a homepage
+def paginainicial():
+    return 'A API está funcionando'
+
+# se ficar criando varias rotas aqui vai ficar parecendo que ta construindo um site, a diferenca duma api pra um site e que a api so retorna a resposta em json da info
+@app.route('/totalvendas') # endpoint da api, onde o usuario recebe de fato a informação
+def totalvendas():
+    tabela = pd.read_csv('dados.csv')
+    total_vendas = tabela['Vendas'].sum()
+    resposta = {'totalvendas': total_vendas} # variavel resposta que guarda a info do total de vendas de fato
+    return jsonify(resposta) # usar o jsonify para sair do dict do py e transformar de fato em json
+    
+    #print(total_vendas) isso n vai existir mais, n se retorna print numa api
+
+# rodando api
+app.run()
