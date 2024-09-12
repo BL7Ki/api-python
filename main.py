cdc1c6e7c19d0a1a1b614400e6a8e5f9089cc4e3
Leonardo Ferreira <leoventura245@gmail.com>
2024-09-12 00:03:58 -0300

    implementando try except

--- main.py
+++ main.py
@@ -11,9 +11,14 @@ def paginainicial():
 # se ficar criando varias rotas aqui vai ficar parecendo que ta construindo um site, a diferenca duma api pra um site e que a api so retorna a resposta em json da info
 @app.route('/totalvendas') # endpoint da api, onde o usuario recebe de fato a informação
 def totalvendas():
-    tabela = pd.read_csv('dados.csv')
-    total_vendas = tabela['Vendas'].sum()
-    resposta = {'totalvendas': total_vendas} # variavel resposta que guarda a info do total de vendas de fato
+    try:
+        tabela = pd.read_csv('dados.csv')
+        total_vendas = tabela['Vendas'].sum()
+        resposta = {'totalvendas': total_vendas} # variavel resposta que guarda a info do total de vendas de fato
+    except FileNotFoundError:
+        resposta = {'erro': 'Arquivo dados.csv não encontrado'}
+    except KeyError:
+        resposta = {'erro': 'Coluna "Vendas" não encontrada no arquivo'}
     return jsonify(resposta) # usar o jsonify para sair do dict do py e transformar de fato em json
     
     #print(total_vendas) isso n vai existir mais, n se retorna print numa api
